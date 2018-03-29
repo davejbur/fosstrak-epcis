c3d5ac08d6e858bcf3a0879875a537527b143389
davejbur <davejbur@users.noreply.github.com>
2018-03-29 15:10:26 +0100

    Updated to latest versions of plugins & dependencies, EXCEPT for hibernate, spring & cxf. This version compiles and works.

--- epcis-repository/src/main/java/org/fosstrak/epcis/repository/StaticContentServlet.java
+++ epcis-repository/src/main/java/org/fosstrak/epcis/repository/StaticContentServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * This is a wrapper for the default Servlet which handles requests to
@@ -17,16 +19,27 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class StaticContentServlet extends HttpServlet {
 
+    private static final Log LOG = LogFactory.getLog(StaticContentServlet.class);
     private static final long serialVersionUID = 1L;
 
+    //FIX - this doesn't seem to work...!
+    @Override
     public void doGet(final HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher rd = getServletContext().getNamedDispatcher("default");
         HttpServletRequest wrapped = new HttpServletRequestWrapper(req) {
             @Override
             public String getPathInfo() {
+                if (LOG.isDebugEnabled())
+                {
+                  LOG.debug("Doing getpathinfo: "+req.getPathInfo());
+                }
                 return "/static" + req.getPathInfo();
             }
         };
+        if (LOG.isDebugEnabled())
+        {
+          LOG.debug("Doing rd.forward: "+wrapped.getPathInfo()+", "+resp.toString());
+        }
         rd.forward(wrapped, resp);
     }
 }
