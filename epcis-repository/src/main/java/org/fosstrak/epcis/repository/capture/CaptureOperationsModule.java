1f168a95cf2fdbd33fc1aebaab0646eee17eea82
davejbur <davejbur@users.noreply.github.com>
2018-04-03 11:32:52 +0100

    Updated to spring 3.2.18.release

--- epcis-repository/src/main/java/org/fosstrak/epcis/repository/capture/CaptureOperationsModule.java
+++ epcis-repository/src/main/java/org/fosstrak/epcis/repository/capture/CaptureOperationsModule.java
@@ -56,7 +56,8 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
-import org.apache.commons.lang.StringUtils;
+//import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.fosstrak.epcis.repository.EpcisConstants;
